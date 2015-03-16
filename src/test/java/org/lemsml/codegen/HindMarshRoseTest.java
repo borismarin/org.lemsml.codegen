ebb70826a50cedb2fa94b019d98eb6514796290c
Boris Marin <borismarin@gmail.com>
2015-03-16 18:38:27 +0000

    initial commit

---
+++ src/test/java/org/lemsml/codegen/HindMarshRoseTest.java
@@ -0,0 +1,42 @@
+package org.lemsml.codegen;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.lemsml.model.ComponentType;
+import org.lemsml.model.compiler.LEMSCompilerFrontend;
+import org.lemsml.model.extended.Lems;
+
+public class HindMarshRoseTest
+{
+	protected File getLocalFile(String fname) {
+		return new File(getClass().getResource(fname).getFile());
+	}
+
+	/**
+	 * @author borismarin
+	 *
+	 */
+
+	private File hindMarshRoseSimFile;
+
+	@Before
+	public void setUp()
+	{
+		hindMarshRoseSimFile = getLocalFile("/examples/nml/Run_Chaotic_HindmarshRose.xml");
+	}
+
+	@Test
+	public void testParsing() throws Throwable
+	{
+
+		LEMSCompilerFrontend compiler = new LEMSCompilerFrontend(hindMarshRoseSimFile);
+
+		Lems lemsDoc = compiler.generateLEMSDocument();
+
+		ComponentType hindRoseCompType = lemsDoc.getComponentTypeByName("hindmarshRoseCell");
+
+	}
+
+}
