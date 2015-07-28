b496e320bf3a98bd2a52afcc5704b1159abe570e
borismarin <borismarin@gmail.com>
2015-07-28 20:06:50 +0100

    Silly rate plotting test

--- src/test/java/org/lemsml/codegen/NMLGen.java
+++ src/test/java/org/lemsml/codegen/NMLGen.java
@@ -1,9 +1,17 @@
 package org.lemsml.codegen;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
+import javax.measure.Quantity;
+import javax.measure.Unit;
+import javax.swing.JFrame;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -13,13 +21,22 @@ import org.junit.Test;
 import org.lemsml.codegen.fakeNML.FakeNML;
 import org.lemsml.codegen.fakeNML.HHChannel;
 import org.lemsml.codegen.fakeNML.HHGate;
+import org.lemsml.codegen.fakeNML.HHRate;
 import org.lemsml.model.compiler.LEMSCompilerFrontend;
 import org.lemsml.model.compiler.semantic.LEMSSemanticAnalyser;
+import org.lemsml.model.exceptions.LEMSCompilerException;
 import org.lemsml.model.extended.Lems;
+import org.lemsml.model.extended.NamedDimensionalType;
+import org.lemsml.model.extended.Scope;
+import org.lemsml.model.extended.Symbol;
+import org.math.plot.Plot2DPanel;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 import org.stringtemplate.v4.STGroupFile;
 import org.stringtemplate.v4.StringRenderer;
+
+import tec.units.ri.quantity.Quantities;
+
 
 public class NMLGen {
 
@@ -56,14 +73,61 @@ public class NMLGen {
 	}
 
 	@Test
-	public void testParsing(){
+	public void testParsing() throws LEMSCompilerException, IOException {
+
 		Set<HHChannel> allChannels = nmlDoc.getAllOfType(HHChannel.class);
-		for(HHChannel chan : allChannels){
-			for(HHGate gate : chan.getGates()){
+		for (HHChannel chan : allChannels) {
+			for (HHGate gate : chan.getGates()) {
 				System.out.println(chan + " " + gate);
+
+				JFrame frame = new JFrame(gate.getName());
+				frame.setSize(800, 600);
+				frame.setVisible(true);
+				plotRate(gate.getForward(), frame);
+				plotRate(gate.getReverse(), frame);
 			}
 		}
+		System.in.read();
+	}
 
+	private void plotRate(HHRate rate, JFrame frame) throws LEMSCompilerException {
+			Plot2DPanel plot = new Plot2DPanel();
+		Scope scope = rate.getScope();
+		int npt = 100;
+		Map<Double, Double> graph = evalInGrid(scope, scope.resolve("r"), scope.resolve("v"), -80., 100., npt);
+		double[] xs = new double[npt];
+		double[] ys = new double[npt];
+		int i = 0;
+		for(Entry<Double, Double> pt : graph.entrySet()){
+			xs[i] = pt.getKey();
+			ys[i] = pt.getValue();
+			i++;
+		}
+		plot.addLinePlot(rate.getName(), xs, ys);
+		frame.setContentPane(plot);
+		frame.setVisible(true);
+	}
+
+	private static Map<Double, Double> evalInGrid(Scope scope,
+			Symbol toEval, Symbol indepVar, Double x0, Double x1,
+			Integer npoints) throws LEMSCompilerException {
+
+		Map<Double, Double> graph = new LinkedHashMap<Double, Double>();
+
+		Unit<?> dim = ((NamedDimensionalType) indepVar.getType())
+				.getUOMDimension();
+		String ivName = indepVar.getName();
+		Map<String, Quantity<?>> ctxt = new HashMap<String, Quantity<?>>();
+
+		Double dx = Math.ceil(Math.abs(x1 - x0) / npoints);
+		for (int i = 0; i < npoints; i++) {
+			Double x = x0 + i * dx;
+			ctxt.put(ivName, Quantities.getQuantity(x, dim));
+			Double fx = scope.evaluate(toEval.getName(), ctxt).getValue()
+					.doubleValue();
+			graph.put(x, fx);
+		}
+		return graph;
 	}
 
 	public ST merge(Lems lems, String langName) {
